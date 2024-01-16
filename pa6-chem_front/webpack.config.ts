20acb50b54ee7136479a2430f1d24262684e23a4
sasha <sayces@mail.ru>
2024-01-16 12:57:41 +0300

    hi again

--- pa6-chem_front/webpack.config.ts
+++ pa6-chem_front/webpack.config.ts
@@ -2,10 +2,19 @@ import path from "path";
 import HTMLWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
 import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+import type { Configuration } from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+const devServer: DevServerConfiguration = {};
+
 
 export default (env: any) => {
-  const config: webpack.Configuration = {
+  
+
+  const config: Configuration = {
+    devServer: {
+      port: 5005,
+      open: true,
+    },
     mode: env.mode ?? "development",
     entry: {
       main: path.resolve(__dirname, "src", "index.tsx"),
@@ -20,21 +29,14 @@ export default (env: any) => {
         title: `youko's chemistry`,
         template: path.resolve(__dirname, "public", "index.html"),
       }),
-      new MiniCssExtractPlugin({
-        path: path.resolve(__dirname, "sass", 'common.scss')
-      })
+      new MiniCssExtractPlugin({}),
     ],
     module: {
       rules: [
         {
           test: /\.scss$/i,
           use: [
-            // Creates `style` nodes from JS strings
-            MiniCssExtractPlugin.loader,
-            // Translates CSS into CommonJS
-            "css-loader",
-            // Compiles Sass to CSS
-            "sass-loader",
+            "style-loader", "css-modules-typescript-loader", "css-loader"
           ],
           exclude: /node_modules/,
         },
@@ -46,11 +48,7 @@ export default (env: any) => {
       ],
     },
     resolve: {
-      extensions: [".scss", ".tsx", ".ts", ".jsx", ".js"],
-    },
-    devServer: {
-      port: 5005,
-      open: true,
+      extensions: [".tsx", ".ts", ".jsx", ".js"],
     },
   };
 
